7769bc56845fb1bce7c535f21f4c8ccaeab23b0a
khushhal kumrawat <khushhalkumrawat@gmail.com>
2025-12-25 13:01:50 +0530

    Update Space Complexity

--- Concatenation of Array/brute.cpp
+++ Concatenation of Array/brute.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 // Brute and Optimized Approach :
 // TIme Complexity : O(n)
-// space Complexity : O(1)
+// space Complexity : O(n)
 
 vector<int> ConcatenationOfArray( vector<int> nums ) {
   
     int n = nums.size();
+    nums.resize(2*n);
 
     for ( int i = 0 ; i < n ; i++){
         nums[i+n] = nums[i];
